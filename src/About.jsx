2cbd88ab321eec43ef1f2ddba15392303e4172f9
Dev <Dev@GP01FB>
2021-10-21 16:59:09 +0700

    Day1

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>Page About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, impedit! Amet impedit accusamus hic odio inventore aut, culpa commodi in ullam vitae, excepturi tempore optio fugit eos assumenda consequuntur blanditiis?</p>
+        </div>
+    )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import About from './About';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <About/>
   </React.StrictMode>,
   document.getElementById('root')
 );
